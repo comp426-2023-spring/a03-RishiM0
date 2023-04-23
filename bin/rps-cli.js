043b0db1219fb16083dfaf561d32e8068ca34e8e
RishiM08 <rishi.mantri0803@gmail.com>
2023-04-23 00:13:36 -0400

    help and rules

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,25 +1,24 @@
+#!/usr/bin/env node
 import minimist from 'minimist';
 import {rps} from '../lib/rpsls.js';
 
 var args = minimist(process.argv.slice(2));
 
 function theHelp() {
-    console.log('Usage: node-rpsls [SHOT]\nPlay the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!\n\n',
-    '  -h, --help        display this help message and exit\n',
-    '  -r, --rules       display the rules and exit\n\nExamples:\n',
-    '  node-rps        Return JSON with single player RPSLS result.\n',
-    '                    e.g. {"player":"rock"}\n',
-    '  node-rps rock   Return JSON with results for RPSLS played against a simulated opponent.\n',
-    '                    e.g {"player":"rock","opponent":"paper","result":"lose"}'
-        );
+	console.log('Usage: node-rps [SHOT]\nPlay Rock Paper Scissors (RPS)\n\n',
+                '  -h, --help      display this help message and exit\n',
+                '  -r, --rules     display the rules and exit\n\nExamples:\n',
+                '  node-rps        Return JSON with single player RPS result.\n',
+                '                  e.g. {"player":"rock"}\n',
+                '  node-rps rock   Return JSON with results for RPS played against a simulated opponent.\n',
+                '                  e.g {"player":"rock","opponent":"scissors","result":"win"}');
 }
 
 function theR() {
-    console.log('Rules for the Lizard-Spock Expansion of Rock Paper Scissors:\n\n',
-    ' - Scissors CUTS Paper\n',
-    ' - Paper COVERS Rock\n',
-    ' - Rock CRUSHES Scissors'
-        );
+    console.log('Rules for Rock Paper Scissors:\n\n',
+    '  - Scissors CUTS Paper\n',
+    '  - Paper COVERS Rock\n',
+    '  - Rock CRUSHES Scissors');
 }
 
 
