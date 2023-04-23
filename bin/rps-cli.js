a07e240d6e7c2bf31eafd91298f6fb47b8093a6a
RishiM08 <rishi.mantri0803@gmail.com>
2023-04-22 23:53:52 -0400

    rps

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,42 +1,48 @@
-#!/usr/bin/env node
 import minimist from 'minimist';
-import { rps } from '../lib/rpsls.js';
+import {rps} from '../lib/rpsls.js';
 
-var args = minimist(process.argv.slice(2));
+let args = minimist(process.argv.slice(2));
+
+function theHelp() {
+    console.log('Usage: node-rpsls [SHOT]\nPlay the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!\n\n',
+    '  -h, --help        display this help message and exit\n',
+    '  -r, --rules       display the rules and exit\n\nExamples:\n',
+    '  node-rps        Return JSON with single player RPSLS result.\n',
+    '                    e.g. {"player":"rock"}\n',
+    '  node-rps rock   Return JSON with results for RPSLS played against a simulated opponent.\n',
+    '                    e.g {"player":"rock","opponent":"paper","result":"lose"}'
+        );
+}
+
+function theR() {
+    console.log('Rules for the Lizard-Spock Expansion of Rock Paper Scissors:\n\n',
+    ' - Scissors CUTS Paper\n',
+    ' - Paper COVERS Rock\n',
+    ' - Rock CRUSHES Scissors'
+        );
+}
+
 
 if (args.h || args.help) {
-	helpM();
-	process.exit(0);
-}
-if (args.r || args.rules) {
-	rulesM();
-	process.exit(0);
-}
-
-function helpM() {
-	console.log('Usage: node-rps [SHOT]\nPlay Rock Paper Scissors (RPS)\n\n',
-                '  -h, --help      display this help message and exit\n',
-                '  -r, --rules     display the rules and exit\n\nExamples:\n',
-                '  node-rps        Return JSON with single player RPS result.\n',
-                '                  e.g. {"player":"rock"}\n',
-                '  node-rps rock   Return JSON with results for RPS played against a simulated opponent.\n',
-                '                  e.g {"player":"rock","opponent":"scissors","result":"win"}');
-}
-function rulesM() {
-        console.log('Rules for Rock Paper Scissors:\n\n',
-                '  - Scissors CUTS Paper\n',
-                '  - Paper COVERS Rock\n',
-                '  - Rock CRUSHES Scissors');
+    theHelp();
+    process.exit(0);
+} else if (args.r || args.rules) {
+    theR();
+    process.exit(0);
 }
 
-var move = args._[0];
+if (args._.length == 0) {
+	let result = rps();
+}
 
+let hand = args._[0];
 try {
-	result = rps(move);
-	console.log(JSON.stringify(result));
-} catch (e) {
-	console.log('Invalid entry!\n\n');
-	helpM();
-	rulesM();
-	process.exit(0);
+    let result = rps(hand);
+    console.log(JSON.stringify(result))
+} catch (err) {
+    console.log(err);
+    console.log('\ninvalid entry\n');
+    theHelp();
+    theR();
+    process.exit(0);
 }
